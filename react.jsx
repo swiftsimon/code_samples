c595ef147b48120dbca3c7926bc271f0acc76146
Simon <sisihamilton@hotmail.com>
2020-03-16 16:20:44 -0700

    adding react code sample

---
+++ react.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./SearchBar.scss";
+
+
+// Render 2 search bars and submit the value to the parent
+// Code from CrowdRecruit App
+class SearchBar extends Component {
+  state = {
+    citySearchTerm: "",
+    titleSearchTerm: ""
+  };
+
+  onCitySearch = e => {
+    e.preventDefault();
+    let cleanedTerm = this.state.citySearchTerm.trim().toLowerCase();
+    this.props.onCitySearch(cleanedTerm);
+  };
+
+  onTitleSearch = e => {
+    e.preventDefault();
+    let cleanedTerm = this.state.titleSearchTerm.trim().toLowerCase();
+    this.props.onTitleSearch(cleanedTerm);
+  };
+
+  render() {
+    return (
+      <div className="search-bar-wrapper">
+        <div className="search-container">
+          <form onSubmit={this.onCitySearch}>
+            <label>City</label>
+            <input
+              type="text"
+              value={this.state.citySearchTerm}
+              onChange={e => this.setState({ citySearchTerm: e.target.value })}
+            />
+          </form>
+        </div>
+        <div className="search-container">
+          <form onSubmit={this.onTitleSearch}>
+            <label>Job Title</label>
+            <input
+              type="text"
+              value={this.state.titleSearchTerm}
+              onChange={e => this.setState({ titleSearchTerm: e.target.value })}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
